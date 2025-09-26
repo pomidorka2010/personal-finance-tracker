3364bb163906f505dad58cc99fbe9d181bebb47e
pomidorka2010 <lamc560ki@hotmail.com>
2025-09-26 13:43:47 +0200

    Update mongoose connection options for deprecation warnings.

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // DB Connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
