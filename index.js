84c14486bce68e0f6816b7f6a4998d7a2927a69f
pomidorka2010 <lamc560ki@hotmail.com>
2025-09-26 13:45:40 +0200

    Update MongoDB connection options to remove deprecated useCreateIndex and useFindAndModify.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
