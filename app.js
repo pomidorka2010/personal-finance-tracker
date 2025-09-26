b2e75f7325cb7e7788bf77c609e6762b0dc01a22
pomidorka2010 <lamc560ki@hotmail.com>
2025-09-26 13:42:45 +0200

    Add CORS middleware for handling cross-origin requests.

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const cors = require('cors');
 const authRoutes = require('./routes/auth');
 const financeRoutes = require('./routes/finance');
 
 const app = express();
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 
 // Routes
