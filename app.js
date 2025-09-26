2b579849711544f258764a8f4609740125aa75d0
pomidorka2010 <lamc560ki@hotmail.com>
2025-09-26 13:43:37 +0200

    Add server listener to app.js

--- app.js
+++ app.js
@@ -15,4 +15,10 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/finance', financeRoutes);
 
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 module.exports = app;
