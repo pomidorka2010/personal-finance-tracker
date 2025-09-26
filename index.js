af893447a5a5bbf598e1e35bef4bb0cbfe03db5e
pomidorka2010 <lamc560ki@hotmail.com>
2025-09-26 13:42:59 +0200

    Updated mongoose connection options to include `useFindAndModify: false`

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
   .then(() => {
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
